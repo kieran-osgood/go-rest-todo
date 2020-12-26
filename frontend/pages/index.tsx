73f9ef9fce0e6ff5999ff2970b53446a642f4bc8
Kieran Osgood <kieran@analogrepublic.com>
2020-12-26 23:11:12 +0000

    feat: setup search input field from frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,22 +1,24 @@
 import * as React from 'react';
 import { useQuery, UseQueryResult } from 'react-query';
 import axios, { AxiosError } from 'axios';
+import { useDebounce } from 'use-debounce';
 import styles from '../styles/Home.module.scss';
 import Layout from '../components/layout';
 
-// const getPostById = async (postId) => {
+const getSearchTodos = async (name: string) => {
+  const { data } = await axios.get(
+    `/api/todos/search?search=${name}`,
+  );
+  return data;
+};
+
+// const getPost = async () => {
 //   const { data } = await axios.get(
-//     `${baseUrl}${postId}`,
+//     '/api/todos',
 //   );
 //   return data;
 // };
 
-const getPost = async () => {
-  const { data } = await axios.get(
-    '/api/todos',
-  );
-  return data;
-};
 type DateTime = {Time: string, Valid: boolean};
 type Todo = {
   CreationTimestamp: DateTime;
@@ -26,11 +28,16 @@ type Todo = {
   Text: string;
 }
 
-const useListTodos = (): UseQueryResult<{data: Todo[]}, AxiosError> => useQuery('search', () => getPost());
+// const useListTodos = (): UseQueryResult<{data: Todo[]}, AxiosError> => { returnuseQuery('search', () => getPost()); };
+
+const useSearchTodos = (searchString: string): UseQueryResult<{data: Todo[]}, AxiosError> => useQuery(['search', searchString], () => getSearchTodos(searchString), { enabled: !!searchString });
 
 export default function Home() {
   const [search, setSearch] = React.useState('');
-  const { data: todoList } = useListTodos();
+  // const { data: todoList } = useListTodos();
+  const [debouncedSearchTerm] = useDebounce(search, 500);
+  const { data: todoList } = useSearchTodos(debouncedSearchTerm);
+
   return (
     <Layout>
       <main className={styles.main}>
