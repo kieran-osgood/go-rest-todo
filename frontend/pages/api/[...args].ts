5781749556eeb0ecd0e58ba14a7c38886a88e297
Kieran Osgood <kieran@analogrepublic.com>
2020-12-26 19:03:13 +0000

    refactor: replace hardcoded proxy url target with .env value

--- frontend/pages/api/[...args].ts
+++ frontend/pages/api/[...args].ts
@@ -2,18 +2,18 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 // Create proxy instance outside of request handler function to avoid unnecessary re-creation
 const apiProxy = createProxyMiddleware({
-  target: 'http://localhost:8080',
+  target: process.env.NEXT_PUBLIC_API_URL,
   changeOrigin: true,
   // pathRewrite: { [`^/api/proxy`]: '' },
   secure: false,
   router: {
     // when request.headers.host == 'dev.localhost:3000',
     // override target 'http://www.example.org' to 'http://localhost:8000'
-    'localhost:3000': 'http://localhost:8080',
+    'localhost:3000': process.env.NEXT_PUBLIC_API_URL,
   },
 });
 
-export default function (req, res) {
+export default (req, res) => {
   apiProxy(req, res, (result) => {
     if (result instanceof Error) {
       throw result;
