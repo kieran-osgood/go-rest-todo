c0712530a36699d914881462b1d56b0778855c67
Kieran Osgood <kieranbosgood@gmail.com>
2021-01-02 14:01:55 +0000

    feat: setup page for integrating C_UD operations

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -12,13 +12,6 @@ const getSearchTodos = async (name: string) => {
   return data;
 };
 
-// const getPost = async () => {
-//   const { data } = await axios.get(
-//     '/api/todos',
-//   );
-//   return data;
-// };
-
 type DateTime = {Time: string, Valid: boolean};
 type Todo = {
   CreationTimestamp: DateTime;
@@ -28,36 +21,79 @@ type Todo = {
   Text: string;
 }
 
-// const useListTodos = (): UseQueryResult<{data: Todo[]}, AxiosError> => { returnuseQuery('search', () => getPost()); };
-
-const useSearchTodos = (searchString: string): UseQueryResult<{data: Todo[]}, AxiosError> => useQuery(['search', searchString], () => getSearchTodos(searchString), { enabled: !!searchString });
+const useSearchTodos = (searchString: string): UseQueryResult<{data: Todo[]}, AxiosError> => useQuery(['search', searchString], () => getSearchTodos(searchString));
 
 export default function Home() {
-  const [search, setSearch] = React.useState('');
-  // const { data: todoList } = useListTodos();
-  const [debouncedSearchTerm] = useDebounce(search, 500);
-  const { data: todoList } = useSearchTodos(debouncedSearchTerm);
-
   return (
     <Layout>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Fuzzy Search
-        </h1>
-        <p className={styles.description}>
-          Fuzzy search:
-        </p>
-
-        <div className={styles.card}>
-          Search:&nbsp;
-          <input type="text" value={search} onChange={(e) => setSearch(e.currentTarget.value)} />
-          <ul>
-            {todoList?.data?.map((todo) => (
-              <li key={todo.ID}>{todo.Text}</li>
-            ))}
-          </ul>
+        <div>
+          <h1 className={styles.title}>
+            To-Do List
+          </h1>
+          <Form />
         </div>
+        <SearchCard />
       </main>
     </Layout>
   );
 }
+const Form = () => (
+  <div style={{ marginTop: '1rem' }}>
+    <form className={styles.form}>
+      <label htmlFor="todo" className={styles.label}>
+        <input type="text" id="todo" name="todo" className={styles.input} />
+      </label>
+      <button type="submit" className={styles.button}>Add</button>
+    </form>
+  </div>
+);
+
+const SearchCard = () => {
+  const [search, setSearch] = React.useState('');
+  const [debouncedSearchTerm] = useDebounce(search, 500);
+
+  const { data: todoList } = useSearchTodos(debouncedSearchTerm);
+  return (
+    <>
+      <div className={styles.card}>
+        <p className={styles.description}>
+          Fuzzy search/filter:
+        </p>
+
+        Search:&nbsp;
+        <input type="text" value={search} onChange={(e) => setSearch(e.currentTarget.value)} />
+        <ul>
+          {todoList?.data?.map((todo) => (
+            <li key={todo.ID}>{todo.Text}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+// const getPost = async () => {
+//   const { data } = await axios.get(
+//     '/api/todos',
+//   );
+//   return data;
+// };
+
+// const useListTodos = (): UseQueryResult<{data: Todo[]}, AxiosError> => {
+// return useQuery('search', () => getPost());
+// }
+
+// const ListTodos = () => {
+//   const { data: todoList } = useListTodos();
+
+//   return (
+//     <div>
+//       <ul>
+//         {todoList?.data?.map((todo) => (
+//           <li key={todo.ID}>{todo.Text}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
